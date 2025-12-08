aa3441f8f98b74e380af0d6ef4e34bc4941ad5f7
Ting Yung Terry Lu <terrylty18@gmail.com>
2025-12-08 17:35:24 -0500

    responsive inventaire

--- ox-bezoar/src/components/layout/InventoryDisplay.vue
+++ ox-bezoar/src/components/layout/InventoryDisplay.vue
@@ -1,15 +1,19 @@
 <script setup>
-    import { mapStores } from 'pinia';
-    import { usePlayerStore } from '../../stores/usePlayerStore';
+import { mapStores } from 'pinia';
+import { usePlayerStore } from '../../stores/usePlayerStore';
 </script>
 
 <template>
     <div class="inventaire">
         <!-- les images de objets sont sombres et deviennent clair si ils sont acquises -->
-        <div class="boiteObjet" :class="{hasobject : dent}"><img src="/src/img/dent_de_requin.png" alt="objet1" class="objet"></div>
-        <div class="boiteObjet" :class="{hasobject : diamant}"><img src="/src/img/diamant.png" alt="objet2" class="objet"></div>
-        <div class="boiteObjet" :class="{hasobject : ourson}"><img src="/src/img/ourson.png" alt="objet3" class="objet"></div>
-        <div class="boiteObjet" :class="{hasobject : oeuf}"><img src="/src/img/oeuf.png" alt="objet4" class="objet"></div>
+        <div class="boiteObjet" :class="{ hasobject: dent }"><img src="/src/img/dent_de_requin.png" alt="objet1"
+                class="objet"></div>
+        <div class="boiteObjet" :class="{ hasobject: diamant }"><img src="/src/img/diamant.png" alt="objet2"
+                class="objet"></div>
+        <div class="boiteObjet" :class="{ hasobject: ourson }"><img src="/src/img/ourson.png" alt="objet3" class="objet">
+        </div>
+        <div class="boiteObjet" :class="{ hasobject: oeuf }"><img src="/src/img/oeuf.png" alt="objet4" class="objet">
+        </div>
     </div>
 </template>
 
@@ -32,7 +36,7 @@ export default {
         this.checkFlags();
     },
     methods: {
-        checkFlags(){
+        checkFlags() {
             this.diamant = this.playerStore.flags.hasDiamant;
         }
     }
@@ -57,12 +61,12 @@ export default {
     border-style: solid;
     border-color: var(--brunsombre);
     border-radius: 5px;
-    filter:grayscale(100);
+    filter: grayscale(100);
 
 }
 
 .hasobject {
-    filter:grayscale(0);
+    filter: grayscale(0);
 }
 
 .objet {
@@ -72,13 +76,25 @@ export default {
 
 @media screen and (max-width: 1000px) {
     .inventaire {
-    width: 200px;
-    gap: 10px;
+        width: 200px;
+        gap: 10px;
+    }
+
+    .boiteObjet {
+        width: 40px;
+        height: 40px;
+    }
 }
 
-.boiteObjet {
-    width: 40px;
-    height: 40px;
-}
+@media screen and (max-width: 600px) {
+    .inventaire {
+        width: 150px;
+        gap: 10px;
+    }
+
+    .boiteObjet {
+        width: 25px;
+        height: 25px;
+    }
 }
 </style>
